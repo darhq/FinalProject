553343fa5bd76e30e57d63977842af7fad507d75
darhq <dariuszhanuszewicz001@gmail.com>
2018-12-17 00:53:22 +0000

    Add files via upload
    Due to the file size just placing the scripts here. The whole project available at https://drive.google.com/drive/folders/1c7d0A_yLDEFlHsK42CDwi7g_W0Y0BH-w?usp=sharing

---
+++ Scripts/Bridge.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bridge : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        { // if player entered the trigger...
+            GetComponent<Rigidbody>().useGravity = true; // turn gravity on
+            GetComponent<Rigidbody>().isKinematic = false;// turn kinematic off
+
+
+            //audio.PlayOneShot(audio.clip, 1.0f); // if player entered the trigger play audio...
+
+
+
+            //Destroy(gameObject, 5.0f); // if player entered the trigger destroy after 5 sec...
+        }
+    }
+}
